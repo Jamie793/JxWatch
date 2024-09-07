299d18b7114b4ddd80085dd653fff062c0341346
Jamiexu <doxm@foxmail.com>
2024-09-07 23:28:18 +0800

    Added testing

--- Software/Test/test_cst816t.c
+++ Software/Test/test_cst816t.c
@@ -1,5 +1,12 @@
 #include "cst816t.h"
+#include "main.h"
+
 void test_cst816t(void){
+    LCD_TP_RST_GPIO_Port->ODR &= ~LCD_TP_RST_Pin;
+    HAL_Delay(500);
+    LCD_TP_RST_GPIO_Port->ODR |= LCD_TP_RST_Pin;
+    HAL_Delay(100);
+
     volatile uint8_t reg = cst816t_read_reg(0xA7);
     reg = 2;
-}
+} 
